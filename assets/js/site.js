f49e967f771968edaa9bff72cd2d6198a5eea627
fedef <fed.ferrari@gmail.com>
2015-01-23 06:32:19 +0100

    jekyll version

---
+++ assets/js/site.js
@@ -0,0 +1,59 @@
+$( document ).ready(function() {
+
+	var copyrightYear = $("#copyright-year").get(0);
+	copyrightYear.innerHTML = getAnno();
+
+	var bordo;
+
+	$("ul#showcase li div").on("mouseenter", function(){
+		$(this).animate({
+			backgroundColor: "rgba(255,255,255,0)"
+			}, 500, function() {
+			// Animation complete.
+		});
+
+	});
+	$("ul#showcase li div").on("mouseleave", function(){
+		$(this).animate({
+			backgroundColor: "rgba(255,255,255,0.4)"
+		}, 100, function() {
+			// Animation complete.
+		});
+
+	});
+/*
+	$items = $("ul#showcase li");
+	$items.on("click", function(){
+		colortmp = $(this).css("background-color");
+		//$("#portfolio").css("background-color", colortmp);
+		$(this).css("background-color", "transparent");
+		$(this).css("border", "1px solid "+colortmp);
+		$("#portfolio").css("background-image", 'url("images/casaenergia.jpg")');
+
+	});
+	*/
+//
+// $("ul#showcase #amministrazionimmobiliari").on("click", function(){
+// 	$("#portfolio").css("background-color", "gray");
+// });
+// $("ul#showcase #commercioequogrosseto").on("click", function(){
+// 	$("#portfolio").css("background-color", "red");
+// });
+// $("ul#showcase #ilsassolinobianco").on("click", function(){
+// 	$("#portfolio").css("background-color", "white");
+// });
+
+//	misura();
+
+});
+
+function misura(){
+	$('.misura').css('color', 'yellow');
+}
+function getAnno(){
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var year = today.getFullYear();
+	return year;
+}
---
+++ php/footer.php
@@ -0,0 +1,19 @@
+	<footer>
+
+		<div id="stripe">
+			<p>fedef.net &copy; 2014 -
+				<a href="mailto:info@fedef.net" title="send an email">
+					<img alt="email" src="style/img/ico_email2.png" />&nbsp;&nbsp;info@fedef.net
+				</a> -
+				<img alt="email" src="style/img/ico_phone.png" />&nbsp;&nbsp;3382067109
+			</p>
+		</div>
+		<div id="internetdefenseleague">
+			<a href="http://internetdefenseleague.org"><img src="images/internet_defense_league_badge.png" alt="Member of The Internet Defense League"/></a>
+		</div>
+	</footer>
+
+	</section>
+
+</body>
+</html>
